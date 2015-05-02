777057651652fe3e26ff9befbbfbb6632844d770
David Irvine <david.irvine@maidsafe.net>
2015-05-02 14:47:48 +0100

    initial commit - dumping ground lots to remove

---
+++ src/messages/connect_response.rs
@@ -0,0 +1,90 @@
+// Copyright 2015 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under (1) the MaidSafe.net Commercial License,
+// version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+// licence you accepted on initial access to the Software (the "Licences").
+//
+// By contributing code to the SAFE Network Software, or to this project generally, you agree to be
+// bound by the terms of the MaidSafe Contributor Agreement, version 1.0.  This, along with the
+// Licenses can be found in the root directory of this project at LICENSE, COPYING and CONTRIBUTOR.
+//
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+//
+// Please review the Licences for the specific language governing permissions and limitations
+// relating to use of the SAFE Network Software.
+
+#![allow(unused_assignments)]
+
+use cbor::CborTagEncode;
+use rustc_serialize::{Decodable, Decoder, Encodable, Encoder};
+use std::net::{SocketAddr};
+use NameType;
+
+use types;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ConnectResponse {
+  pub requester_local    : SocketAddr,
+  pub requester_external : SocketAddr,
+  pub receiver_local     : SocketAddr,
+  pub receiver_external  : SocketAddr,
+  pub requester_id       : NameType,
+  pub receiver_id        : NameType,
+  pub receiver_fob       : types::PublicPmid
+}
+
+impl Encodable for ConnectResponse {
+  fn encode<E: Encoder>(&self, e: &mut E)->Result<(), E::Error> {
+    // FIXME: Implement Encodable/Decodable for SocketAddr
+    let requester_local    = format!("{}", self.requester_local);
+    let requester_external = format!("{}", self.requester_external);
+    let receiver_local     = format!("{}", self.receiver_local);
+    let receiver_external  = format!("{}", self.receiver_external);
+
+    CborTagEncode::new(5483_001, &(&requester_local, &requester_external,
+                                   &receiver_local, &receiver_external,
+                                   &self.requester_id, &self.receiver_id,
+                                   &self.receiver_fob)).encode(e)
+  }
+}
+
+impl Decodable for ConnectResponse {
+  fn decode<D: Decoder>(d: &mut D)->Result<ConnectResponse, D::Error> {
+    try!(d.read_u64());
+    let (requester_local, requester_external, receiver_local, receiver_external,
+         requester_id, receiver_id, receiver_fob):
+        (String, String, String, String, NameType, NameType, types::PublicPmid)
+        = try!(Decodable::decode(d));
+
+    let req_local    = try!(requester_local   .parse().or(Err(d.error("can't parse req_local addr"))));
+    let req_external = try!(requester_external.parse().or(Err(d.error("can't parse req_external addr"))));
+    let rec_local    = try!(receiver_local    .parse().or(Err(d.error("can't parse rec_local addr"))));
+    let rec_external = try!(receiver_external .parse().or(Err(d.error("can't parse rec_external addr"))));
+
+    Ok(ConnectResponse { requester_local: req_local, requester_external: req_external,
+                         receiver_local: rec_local, receiver_external: rec_external,
+                         requester_id: requester_id, receiver_id: receiver_id, receiver_fob: receiver_fob})
+  }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use cbor;
+    use test_utils::Random;
+
+    #[test]
+    fn connect_response_serialisation() {
+        let obj_before: ConnectResponse = Random::generate_random();
+
+        let mut e = cbor::Encoder::from_memory();
+        e.encode(&[&obj_before]).unwrap();
+
+        let mut d = cbor::Decoder::from_bytes(e.as_bytes());
+        let obj_after: ConnectResponse = d.decode().next().unwrap().unwrap();
+
+        assert_eq!(obj_before, obj_after);
+    }
+}
---
+++ src/messages/get_group_key_response.rs
@@ -0,0 +1,135 @@
+// Copyright 2015 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under (1) the MaidSafe.net Commercial License,
+// version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+// licence you accepted on initial access to the Software (the "Licences").
+//
+// By contributing code to the SAFE Network Software, or to this project generally, you agree to be
+// bound by the terms of the MaidSafe Contributor Agreement, version 1.0.  This, along with the
+// Licenses can be found in the root directory of this project at LICENSE, COPYING and CONTRIBUTOR.
+//
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+//
+// Please review the Licences for the specific language governing permissions and limitations
+// relating to use of the SAFE Network Software.
+
+#![allow(unused_assignments)]
+
+use cbor::CborTagEncode;
+use rustc_serialize::{Decodable, Decoder, Encodable, Encoder};
+use frequency::{Frequency};
+use types::{PublicSignKey, GROUP_SIZE, QUORUM_SIZE, Mergeable};
+use NameType;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
+pub struct GetGroupKeyResponse {
+  pub public_sign_keys : Vec<(NameType, PublicSignKey)>
+}
+
+impl Mergeable for GetGroupKeyResponse {
+    fn merge<'a, I>(xs: I) -> Option<Self> where I: Iterator<Item=&'a Self> {
+        let mut frequency = Frequency::new();
+
+        for response in xs {
+            for public_sign_key in &response.public_sign_keys {
+                frequency.update(public_sign_key.clone());
+            }
+        }
+
+        let merged_group = frequency.sort_by_highest().into_iter()
+                           .filter(|&(_, ref count)| *count >= QUORUM_SIZE as usize)
+                           .take(GROUP_SIZE as usize)
+                           .map(|(k, _)| k)
+                           .collect::<Vec<_>>();
+
+        if merged_group.is_empty() { return None; }
+        Some(GetGroupKeyResponse{ public_sign_keys: merged_group })
+    }
+}
+
+impl Encodable for GetGroupKeyResponse {
+  fn encode<E: Encoder>(&self, e: &mut E)->Result<(), E::Error> {
+    CborTagEncode::new(5483_001, &self.public_sign_keys).encode(e)
+  }
+}
+
+impl Decodable for GetGroupKeyResponse {
+  fn decode<D: Decoder>(d: &mut D)->Result<GetGroupKeyResponse, D::Error> {
+    try!(d.read_u64());
+    let public_sign_keys = try!(Decodable::decode(d));
+    Ok(GetGroupKeyResponse { public_sign_keys: public_sign_keys})
+  }
+}
+
+#[cfg(test)]
+mod test {
+    use types;
+    use super::*;
+    use cbor;
+    use NameType;
+    use types::{PublicSignKey, GROUP_SIZE, QUORUM_SIZE};
+    use test_utils::Random;
+    use rand::{thread_rng, Rng};
+    use rand::distributions::{IndependentSample, Range};
+
+    #[test]
+    fn get_group_key_response_serialisation() {
+        let obj_before : GetGroupKeyResponse = Random::generate_random();
+
+        let mut e = cbor::Encoder::from_memory();
+        e.encode(&[&obj_before]).unwrap();
+
+        let mut d = cbor::Decoder::from_bytes(e.as_bytes());
+        let obj_after: GetGroupKeyResponse = d.decode().next().unwrap().unwrap();
+
+        assert_eq!(obj_before, obj_after);
+    }
+
+    #[test]
+    fn merge() {
+        let keys: GetGroupKeyResponse = Random::generate_random();
+        // keys.public_sign_keys.len() == types::GROUP_SIZE + 7
+        assert!(keys.public_sign_keys.len() >= GROUP_SIZE as usize);
+
+        let group_size = GROUP_SIZE as usize;
+        let quorum_size = QUORUM_SIZE as usize;
+
+        // get random GROUP_SIZE groups
+        let mut sign_keys = Vec::<(NameType, PublicSignKey)>::with_capacity(quorum_size);
+        let mut rng = thread_rng();
+        let range = Range::new(0, keys.public_sign_keys.len());
+
+        loop {
+            let index = range.ind_sample(&mut rng);
+            if sign_keys.contains(&keys.public_sign_keys[index]) { continue; }
+            sign_keys.push(keys.public_sign_keys[index].clone());
+            if sign_keys.len() == quorum_size { break; }
+        };
+
+        let mut responses = Vec::<GetGroupKeyResponse>::with_capacity(quorum_size);
+
+        for _ in 0..quorum_size {
+            let mut response = GetGroupKeyResponse{ public_sign_keys: Vec::new() };
+            // Take the first QUORUM_SIZE as common...
+            for i in 0..quorum_size {
+                response.public_sign_keys.push(sign_keys[i].clone());
+            }
+            // ...and the remainder arbitrary
+            for _ in quorum_size..group_size {
+                response.public_sign_keys.push((NameType::generate_random(), PublicSignKey::generate_random()));
+            }
+
+            rng.shuffle(&mut response.public_sign_keys[..]);
+            responses.push(response);
+        }
+
+        let merged = types::Mergeable::merge(responses.iter());
+        assert!(merged.is_some());
+        let merged_response = merged.unwrap();
+        for i in 0..quorum_size {
+            assert!(sign_keys.iter().find(|a| **a == merged_response.public_sign_keys[i]).is_some());
+        }
+    }
+}
