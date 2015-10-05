17a94df4a8d1590d2f7e75a26b17805691cdb504
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-10-05 14:52:27 +0100

    Updated lint checks and README.md.

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,13 @@
 //! Sentinel cryptographically confirms the origin of a claim in a decentralised network.
 //!
 //! A claim has to implement Claimable in order to be verifiable and mergeable.
-//! A request has to implement GetSigningKeys such that Sentinel can aqcuire
+//! A request has to implement GetSigningKeys such that Sentinel can acquire
 //! the necessary public signing keys.
 //! The request is passed immutably through sentinel
 //! and is used as a key to group claims and corresponding keys.
 //! When sentinel resolves a threshold on verified and merged message,
-//! it returns the requestor key and the merged claim.
-//! Claimant names and associated signatures are discarded after succesful resolution,
+//! it returns the requester key and the merged claim.
+//! Claimant names and associated signatures are discarded after successful resolution,
 //! as such abstracting the original request into a resolved claim.
 //!
 //! The keys_threshold specifies a minimal threshold on the number of independent mentions of
@@ -32,7 +32,25 @@
 //! can be one or higher.
 //! The claims_threshold specifies a minimal threshold on the number of verified claims before
 //! sentinel will attempt to merge these verified claims.
-#![forbid(bad_style, warnings)]
+
+#![doc(html_logo_url =
+           "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
+       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
+       html_root_url = "http://maidsafe.github.io/sentinel")]
+
+// For explanation of lint checks, run `rustc -W help` or see
+// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
+#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, raw_pointer_derive, stable_features,
+        unconditional_recursion, unknown_lints, unsafe_code, unused, unused_allocation,
+        unused_attributes, unused_comparisons, unused_features, unused_parens, while_true)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results, variant_size_differences)]
+#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
+         missing_debug_implementations)]
 
 extern crate rustc_serialize;
 extern crate accumulator;
